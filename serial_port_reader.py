d147cb7d36612b48b51207f078ff7e7606fb5f61
piotr233 <pdrabik223@gmail.com>
2023-07-10 07:33:59 +0200

    simulation test suite

--- serial_port_reader.py
+++ serial_port_reader.py
@@ -77,6 +77,7 @@ class Board:
         self.starting_positions = []
         self.moves = []
         self.moves_estimation = []
+        self.color_to_move = ''
 
     def display_using_unicode(self):
         starting_positions_highlight_color = Back.GREEN
@@ -105,11 +106,16 @@ class Board:
                 
             if id in self.moves:
                 background_color = moves_highlight_color + moves_highlight_color
-
-            if id in self.moves:
-                if self.moves_estimation[self.moves.index(id)] == max(self.moves_estimation):
-                    background_color = best_move_color + best_move_color
-                    
+            if self.color_to_move == 'white':
+                if id in self.moves:
+                    if self.moves_estimation[self.moves.index(id)] == max(self.moves_estimation):
+                        background_color = best_move_color + best_move_color
+            elif self.color_to_move == 'black':
+                if id in self.moves:
+                    if self.moves_estimation[self.moves.index(id)] == min(self.moves_estimation):
+                        background_color = best_move_color + best_move_color
+                        
+                        
             if to_8_counter == 7:
                 endline = "\n"
 
@@ -274,7 +280,63 @@ class TestCase:
             starting_positions=starting_positions,
             moves=moves,
         )
+class SimulateGameTestCase:
+    def __init__(
+        self,
+        iteration,
+        color,
+        board,
+        position_estimation,
+        no_moves,
+        starting_positions,
+        moves,
+    ) -> None:
+        self.iteration: int = iteration
+        self.board: Board = board
+        self.color:str = color
+        self.position_estimation: float = position_estimation
+        self.no_moves: int = no_moves
+        self.starting_positions: list[int] = starting_positions
+        self.moves: list[int] = moves
 
+    @staticmethod
+    def parse_string(test_str: str):
+        test_params = test_str.split(":")
+        test_params = [param.strip() for param in test_params]
+        test_params = [param.replace("\r", "") for param in test_params]
+        test_params = [param.replace("\n", " ") for param in test_params]
+        test_params = [param for param in test_params if param != ""]
+        
+        for id, param in enumerate(test_params):
+            print(id, param)
+
+        board = Board()
+        board.parse_board(test_params[5])
+
+        starting_positions = []
+        moves = []
+        moves_estimation = []
+
+        no_moves = int(test_params[9])
+        if no_moves > 0:
+            starting_positions = [int(pos) for pos in test_params[11].split(" ")]
+            moves = [int(pos) for pos in test_params[13].split(" ")]
+            moves_estimation = [float(pos) for pos in test_params[15].split(" ")]
+            
+            board.starting_positions = starting_positions
+            board.moves_estimation = moves_estimation
+            board.moves = moves
+            board.color_to_move = test_params[3]
+            
+        return SimulateGameTestCase(
+            iteration=int(test_params[1]),
+            color=test_params[3],
+            board=board,
+            position_estimation=float(test_params[7]),
+            no_moves=no_moves,
+            starting_positions=starting_positions,
+            moves=moves,
+        )
 
 if __name__ == "__main__":
     device = set_up_serial_connection()
@@ -282,10 +344,10 @@ if __name__ == "__main__":
     while True:
         resp: bytes = device.readline()
         try:
-            # print(resp.decode("utf-8"))
-            if "test_end" in resp.decode("utf-8"):
+            print(resp.decode("utf-8"))
+            if "test_end"  in resp.decode("utf-8") or "iteration_end" in resp.decode("utf-8"):
                 buffer = buffer + resp.decode("utf-8")
-                test_example = TestCase.parse_string(buffer)
+                test_example = SimulateGameTestCase.parse_string(buffer)
                 test_example.board.display_using_unicode()
                 buffer = ""
             else:
